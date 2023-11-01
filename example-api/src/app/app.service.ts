7caa2321c511d088d09e3651b4899fabe7ef9104
cberthold <cberthold@technodotnet.com>
2023-10-31 21:35:03 -0400

    add random number calls

--- example-api/src/app/app.service.ts
+++ example-api/src/app/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+export interface GetDataResponse {
+  randomNumber: number[];
+}
+
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return ({ message: 'Hello API' });
+   async getData(): Promise<GetDataResponse> {
+    const url = new URL("https://www.randomnumberapi.com/api/v1.0/randomredditnumber?min=100&max=1000&count=1")
+    const response = await fetch(url);
+    const j = await response.json();
+    console.log(j);
+    return ({ randomNumber: j });
   }
 }
